a88b0c89d52a87456ada9ba6f0af59de6b8ecfcd
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-09-24 21:04:30 +0300

    finishes currency-converter

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 `()=>window.location.reload()` - перезагрузка страницы
 
-`<a href="/react-beginner-projects/quiz"><button>Try again</button></a>` - возвращение на страницу
-
 ### https://skeletonreact.com
 
 `import ContentLoader from 'react-content-loader'` - лоадер
--- src/pages/CurrencyConverterPage.js
+++ src/pages/CurrencyConverterPage.js
@@ -1,15 +1,70 @@
-import { Container } from '../components/Container';
-import { Block } from '../components/Block';
-import '../styles/currency-converter.scss';
+import { useState, useEffect, useRef } from "react";
+import { Container } from "../components/Container";
+import { Block } from "../components/Block";
+import "../styles/currency-converter.scss";
 
 export function CurrencyConverterPage() {
+  // const [exchangeRate, setExchangeRates] = useState({});
+  const exchangeRateRef = useRef({})
+
+  const [selectedCurrency, setSelectedCurrency] = useState("UAH");
+  const [convertedCurrency, setСonvertedCurrency] = useState("USD");
+
+  const [selectedValue, setSelectedValue] = useState(0);
+  const [convertedValue, setСonvertedValue] = useState(1);
+
+  // получение данных по курсу валют
+  useEffect(() => {
+    fetch("https://cdn.cur.su/api/latest.json")
+      .then((res) => res.json())
+      // .then((json) => setExchangeRates(json.rates))
+      .then((json) => {exchangeRateRef.current= json.rates;
+        onChangeСonvertedValue(1)
+      })
+      .catch((err) => console.warn(err));
+  }, []);
+
+  const onChangeSelectedValue = (value) => {
+    const sum = value / exchangeRateRef.current[selectedCurrency];
+    const result = sum * exchangeRateRef.current[convertedCurrency];
+
+    setСonvertedValue(result.toFixed(3));
+    setSelectedValue(value);
+  };
+
+  const onChangeСonvertedValue = (value) => {
+    const result =
+      (exchangeRateRef.current[selectedCurrency] / exchangeRateRef.current[convertedCurrency]) *
+      value;
+
+    setSelectedValue(result.toFixed(3));
+    setСonvertedValue(value);
+  };
+
+  useEffect(() => {
+    onChangeSelectedValue(selectedValue);
+  }, [selectedCurrency]);
+
+  useEffect(() => {
+    onChangeСonvertedValue(convertedValue);
+  }, [convertedCurrency]);
 
   return (
     <Container>
-
-    <div className="CurrencyConverterPage">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
-    </div></Container>
+      <div className="CurrencyConverterPage">
+        <Block
+          currency={selectedCurrency}
+          onChangeCurrency={setSelectedCurrency}
+          value={selectedValue}
+          onChangeValue={onChangeSelectedValue}
+        />
+        <Block
+          value={convertedValue}
+          currency={convertedCurrency}
+          onChangeCurrency={setСonvertedCurrency}
+          onChangeValue={onChangeСonvertedValue}
+        />
+      </div>
+    </Container>
   );
 }
