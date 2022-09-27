827f3dd1d5b588cd34e9f10ff5ab0fa509ed7ed0
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-09-27 20:47:07 +0300

    finishes photo-search

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-`()=>window.location.reload()` - перезагрузка страницы
+`() => window.location.reload()` - перезагрузка страницы
 
 ### https://skeletonreact.com
 
 `import ContentLoader from 'react-content-loader'` - лоадер
+
+### https://mockapi.io/projects/62bb2e96573ca8f83294af76
+https://62bb2e96573ca8f83294af75.mockapi.io/photo-collection
+
--- src/pages/PhotoSearchPage.js
+++ src/pages/PhotoSearchPage.js
@@ -1,38 +1,91 @@
-import { Container } from '../components/Container';
-import '../styles/photo-search.scss';
+import { useState, useEffect } from "react";
+import { Container } from "../components/Container";
+import "../styles/photo-search.scss";
+
+const categoriesList = [
+  { name: "All" },
+  { name: "Sea" },
+  { name: "Mountains" },
+  { name: "Architecture" },
+  { name: "Cities" },
+];
 
 export function PhotoSearchPage() {
+  const [collection, setCollection] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [category, setCategory] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const selectedCategory = category ? `category=${category}` : "";
+
+    setIsLoading(true);
+    fetch(
+      `https://62bb2e96573ca8f83294af75.mockapi.io/photo-collection?page=${page}&limit=3&${selectedCategory}`
+    )
+      .then((res) => res.json())
+      .then((json) => setCollection(json))
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
+  }, [category, page]);
+
   return (
     <Container>
-    <div className="PhotoSearchPage">
-      <h1>My photo collection</h1>
-      <div className="top">
-        <ul className="tags">
-          <li className="active">All</li>
-          <li>Mountains</li>
-          <li>Sea</li>
-          <li>Architecture</li>
-          <li>Cities</li>
+      <div className="PhotoSearchPage">
+        <h1>My photo collection</h1>
+        <div className="top">
+          <ul className="tags">
+            {categoriesList.map(({ name }, index) => (
+              <li
+                key={name}
+                onClick={() => setCategory(index)}
+                className={category === index ? "active" : ""}
+              >
+                {name}
+              </li>
+            ))}
+          </ul>
+
+          <input
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            className="search-input"
+            placeholder="Search by name"
+          />
+        </div>
+
+        <div className="content">
+          {isLoading ? (
+            <h2>Loading... </h2>
+          ) : (
+            collection
+              .filter(({ name }) =>
+                name.toLowerCase().includes(searchValue.toLowerCase())
+              )
+              .map(({ name, photos }, index) => (
+                <Collection
+                  name={name}
+                  images={photos}
+                  key={`${index + name}`}
+                />
+              ))
+          )}
+        </div>
+
+        <ul className="pagination">
+          {[...Array(5)].map((_, index) => (
+            <li
+              onClick={() => setPage(index + 1)}
+              key={index}
+              className={page === index + 1 ? "active" : ""}
+            >
+              {index + 1}
+            </li>
+          ))}
         </ul>
-        <input className="search-input" placeholder="Search by name" />
       </div>
-      <div className="content">
-        <Collection
-          name="Travel around the world"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
-      </div>
-      <ul className="pagination">
-        <li>1</li>
-        <li className="active">2</li>
-        <li>3</li>
-      </ul>
-    </div></Container>
+    </Container>
   );
 }
 
